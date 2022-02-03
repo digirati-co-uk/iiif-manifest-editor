7b0975ea56e051aeebb9f6b5934bec5e19a0958e
Kelsie <kelsie.braidwood@digirati.com>
2022-02-03 09:11:09 +0000

    Linting rules fix

--- manifest-editor/components/organisms/CanvasView.tsx
+++ manifest-editor/components/organisms/CanvasView.tsx
@@ -1,15 +1,15 @@
 import React, { useRef, useEffect } from "react";
 import { useCanvas } from "@hyperion-framework/react-vault";
 
-import {CanvasContainer} from '../atoms/CanvasContainer'
+import { CanvasContainer } from "../atoms/CanvasContainer";
 
 export const CanvasView: React.FC = () => {
   const viewer = useRef();
   const canvas = useCanvas();
   // required for next js ssr
   useEffect(() => {
-    import("@digirati/canvas-panel-web-components")
-  }, [])
+    import("@digirati/canvas-panel-web-components");
+  }, []);
 
   return (
     <CanvasContainer>
