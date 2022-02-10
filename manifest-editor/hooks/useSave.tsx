015323fd492e939b68cabc864c8ddc23f5e32b09
Kelsie <kelsie.braidwood@digirati.com>
2022-02-10 12:10:28 +0000

    roll-back changes

--- manifest-editor/hooks/useSave.tsx
+++ manifest-editor/hooks/useSave.tsx
@@ -1,3 +1,10 @@
+const PERSISTENCEURL = {
+  prod: "https://iiif-preview.stephen.wf/store",
+  // Need iiif-preview to be running locally.
+  local: "http://127.0.0.1:8787/store"
+};
+
+
 export const useSave = async (manifest: any) => {
   const requestOptions = {
     method: "POST",
@@ -7,7 +14,7 @@ export const useSave = async (manifest: any) => {
     body: JSON.stringify(manifest)
   };
   let responseData = {};
-  await fetch("/api/save/", requestOptions)
+  await fetch(PERSISTENCEURL.prod, requestOptions)
     .then(response => {
       return response.json().catch(err => {
         console.error(`'${err}' happened!`);
