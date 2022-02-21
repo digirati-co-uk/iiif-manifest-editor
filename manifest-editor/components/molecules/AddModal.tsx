00f125951de46f93c6b2b81ba4c113891ce009ca
Kelsie <kelsie.braidwood@digirati.com>
2022-02-21 17:46:42 +0000

    Bring in jest testing and strip out boilerplate storybook

---
+++ manifest-editor/components/molecules/AddModal.tsx
@@ -0,0 +1,32 @@
+import { Input } from "../form/Input";
+import { Button } from "../atoms/Button";
+import { CloseIcon } from "../icons/CloseIcon";
+import { useState } from "react";
+
+import { ModalBackground } from "../layout/ModalBackground";
+import { ModalContainer } from "../layout/ModalContainer";
+
+export const AddModal: React.FC<{
+  manifest: string;
+  onChange: any;
+  close: any;
+}> = ({ manifest, onChange, close }) => {
+  const [inputValue, setInputValue] = useState(manifest);
+
+  return (
+    <>
+      <ModalBackground />
+      <ModalContainer>
+        <h2>Import an existing IIIF manifest to get started</h2>
+        <Input
+          placeholder={manifest}
+          onChange={(e: any) => setInputValue(e.target.value)}
+        />
+        <Button onClick={() => onChange(inputValue)}>Load Manifest</Button>
+        <Button onClick={close}>
+          <CloseIcon />
+        </Button>
+      </ModalContainer>
+    </>
+  );
+};
---
+++ manifest-editor/helpers/analyse.ts
@@ -0,0 +1,49 @@
+import { LanguageProperty } from "@iiif/presentation-2";
+
+export type Image = {
+  id: string,
+  type: "Image",
+  format: string,
+  width: number,
+  height: number
+}
+
+export type Manifest = {
+  id: string,
+  type: "Manifest",
+  label: LanguageProperty
+}
+
+export type Collection = {
+  id: string,
+  type: "Collection",
+  label: LanguageProperty
+}
+
+interface State<T> {
+  data?: T;
+  error?: Error;
+}
+export const analyse = async (url: string, expectedTypes?: Array<"Image" | "ImageService" | "Manifest" | "Collection">) => {
+  let data: any = {};
+  if (!url) return;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    data = (await response.json()) as T;
+    console.log(data.response);
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    id: url,
+    type: "Image",
+    // format: string,
+    // width: number,
+    // height: number
+  }
+};
