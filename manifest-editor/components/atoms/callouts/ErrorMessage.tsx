e0327e21a050c0c5dab90f7e0fed86b87ffbb099
Kelsie <kelsie.braidwood@digirati.com>
2022-03-07 14:19:17 +0000

    Bring across WarningMessage from Madoc

---
+++ manifest-editor/components/atoms/callouts/ErrorMessage.tsx
@@ -0,0 +1,16 @@
+import styled, { css } from "styled-components";
+
+export const ErrorMessage = styled.div<{ $small?: boolean }>`
+  background: #a90e21;
+  color: #fff;
+  width: 100%;
+  padding: 0.5em 1em;
+  line-height: 1.9em;
+  ${(props) =>
+    props.$small &&
+    css`
+      font-size: 0.75em;
+      padding: 0.25em 0.5em;
+      line-height: 1.6em;
+    `}
+`;
