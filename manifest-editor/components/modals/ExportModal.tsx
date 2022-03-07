191dded186d930819ca0308a9ace80ad791cd47a
Kelsie <kelsie.braidwood@digirati.com>
2022-03-07 15:42:22 +0000

    Export modal complete

--- manifest-editor/components/modals/ExportModal.tsx
+++ manifest-editor/components/modals/ExportModal.tsx
@@ -1,5 +1,5 @@
-import { useState, useRef } from "react";
-import { Button, CalltoButton, SecondaryButton } from "../atoms/Button";
+import { useState, useRef, useEffect } from "react";
+import { Button, CalltoButton } from "../atoms/Button";
 import { CloseIcon } from "../icons/CloseIcon";
 import { ModalBackground } from "../layout/ModalBackground";
 import { ModalContainer } from "../layout/ModalContainer";
@@ -20,15 +20,56 @@ export const ExportModal: React.FC<{
   const [copySuccess, setCopySuccess] = useState("");
   const textAreaRef = useRef(null);
   const manifest = useManifest();
+  const vault = useVault();
+
+  useEffect(() => {
+    setCopySuccess("");
+  }, [selected]);
 
   function copyToClipboard() {
+    const man =
+      selected === 1 && manifest ? vault.toPresentation2(manifest) : manifest;
+
     if (textAreaRef && textAreaRef.current) {
-      navigator.clipboard.writeText(JSON.stringify(manifest));
+      navigator.clipboard.writeText(JSON.stringify(man));
       setCopySuccess("Copied!");
     }
   }
 
-  const vault = useVault();
+  const downloadFile = (
+    data: any,
+    fileName: string,
+    fileType: "text/json"
+  ): void => {
+    // Create a blob with the data we want to download as a file
+    const blob = new Blob([data], { type: fileType });
+    // Create an anchor element and dispatch a click event on it
+    // to trigger a download
+    const a = document.createElement("a");
+    a.download = fileName;
+    a.href = window.URL.createObjectURL(blob);
+    const clickEvt = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
+    a.dispatchEvent(clickEvt);
+    a.remove();
+  };
+
+  const exportToJson = (e: any) => {
+    if (!manifest) return;
+    e.preventDefault();
+    downloadFile(
+      JSON.stringify(
+        selected === 1 && manifest ? vault.toPresentation2(manifest) : manifest
+      ),
+      //  defaulting to manifest id for the filename but we can change this.
+      manifest.id + ".json",
+      "text/json"
+    );
+  };
+
   return (
     <>
       <ModalBackground />
@@ -55,6 +96,7 @@ export const ExportModal: React.FC<{
                   component: (
                     <JSONPreview>
                       <pre
+                        ref={textAreaRef}
                         dangerouslySetInnerHTML={{
                           __html: JSON.stringify(manifest, null, 2),
                         }}
@@ -67,6 +109,7 @@ export const ExportModal: React.FC<{
                   component: (
                     <JSONPreview>
                       <pre
+                        ref={textAreaRef}
                         dangerouslySetInnerHTML={{
                           __html: JSON.stringify(
                             vault.toPresentation2(manifest),
@@ -86,16 +129,22 @@ export const ExportModal: React.FC<{
         </FlexContainerColumn>
 
         <FlexContainer style={{ justifyContent: "flex-end" }}>
-          <SecondaryButton onClick={() => close()}>CANCEL</SecondaryButton>
           <CalltoButton
             onClick={() => {
-              // savePermalink();
-              // setSaveClicked(true);
+              copyToClipboard();
             }}
           >
-            SAVE
+            COPY TO CLIPBOARD
+          </CalltoButton>
+          <CalltoButton
+            onClick={(e: any) => {
+              exportToJson(e);
+            }}
+          >
+            DOWNLOAD JSON
           </CalltoButton>
         </FlexContainer>
+        <small>{copySuccess}</small>
       </ModalContainer>
     </>
   );
