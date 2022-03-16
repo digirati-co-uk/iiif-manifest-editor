8308f8a6af4f0f922feb9cc3a2b6a95891998f4e
Kelsie <kelsie.braidwood@digirati.com>
2022-03-16 14:37:54 +0000

    switch to load via resource ID

--- manifest-editor/components/apps/Widgets/RecentFiles.tsx
+++ manifest-editor/components/apps/Widgets/RecentFiles.tsx
@@ -1,15 +1,12 @@
 import { useContext } from "react";
 import ShellContext from "../Shell/ShellContext";
-import { useVault } from "react-iiif-vault";
 import { Button } from "../../atoms/Button";
 
 export const RecentFiles: React.FC = () => {
   const shellContext = useContext(ShellContext);
-  const vault = useVault();
-  // Build on this just showing a list for now but we want this to be interactive, thumbnails perhaps?
 
   const handleClick = async (id: string) => {
-    await vault.load(id);
+    shellContext?.changeResourceID(id);
     shellContext?.changeSelectedApplication("ManifestEditor");
   };
 
