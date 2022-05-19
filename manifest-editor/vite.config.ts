49aab6fda428d7f7ca8702eb2195a5971f871bb9
Stephen Fraser <stephen.fraser@digirati.com>
2022-05-19 10:04:51 +0100

    Fixed build

--- manifest-editor/vite.config.ts
+++ manifest-editor/vite.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: "classic",
-    }),
-  ],
+  plugins: [react()],
   test: {
     environment: "happy-dom", // or 'jsdom', 'node'
     globals: true,
