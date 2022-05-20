f96060cc3eb97554018c64777c4f1572f7033b17
Kelsie <kelsie.braidwood@digirati.com>
2022-05-20 16:54:07 +0100

    Put the splash screen into a center panel of the layout

--- manifest-editor/src/apps/Splash/Splash.tsx
+++ manifest-editor/src/apps/Splash/Splash.tsx
@@ -4,24 +4,48 @@ import { RecentFiles } from "../../components/widgets/RecentFiles";
 import { NewTemplates } from "../../components/widgets/NewTemplates";
 import { PaddingComponentLarge } from "../../atoms/PaddingComponent";
 import { LoadManifest } from "../../components/widgets/LoadManifest";
+import { Layout } from "../../shell/Layout/Layout";
+import { LayoutPanel } from "../../shell/Layout/Layout.types";
+import { ManifestEditorProvider } from "../ManifestEditor/ManifestEditor.context";
+
+const shellContext = useShell();
+
+const handleClick = (id: string) => {
+  shellContext.changeResourceID(id);
+  shellContext.changeSelectedApplication("ManifestEditor");
+};
+const leftPanels: LayoutPanel[] = [];
+
+const centerPanels: LayoutPanel[] = [
+  {
+    id: "current-canvas",
+    label: "Current canvas",
+    icon: "",
+    render: () => (
+      <FlexContainerColumn justify="flex-start" style={{ width: "80%", margin: "auto" }}>
+        <PaddingComponentLarge />
+        <LoadManifest />
+        <PaddingComponentLarge />
+        <NewTemplates newTemplates={shellContext.newTemplates} changeManifest={handleClick} />
+        <PaddingComponentLarge />
+        <RecentFiles recentManifests={shellContext.recentManifests} changeManifest={handleClick} />
+        <PaddingComponentLarge />
+      </FlexContainerColumn>
+    ),
+  },
+];
+
+const rightPanels: LayoutPanel[] = [];
 
 export function Splash() {
-  const shellContext = useShell();
-
-  const handleClick = (id: string) => {
-    shellContext.changeResourceID(id);
-    shellContext.changeSelectedApplication("ManifestEditor");
-  };
-
   return (
-    <FlexContainerColumn justify="flex-start" style={{ width: "80%", margin: "auto" }}>
-      <PaddingComponentLarge />
-      <LoadManifest />
-      <PaddingComponentLarge />
-      <NewTemplates newTemplates={shellContext.newTemplates} changeManifest={handleClick} />
-      <PaddingComponentLarge />
-      <RecentFiles recentManifests={shellContext.recentManifests} changeManifest={handleClick} />
-      <PaddingComponentLarge />
-    </FlexContainerColumn>
+    <ManifestEditorProvider defaultLanguages={[]} behaviorProperties={[]}>
+      <Layout
+        //
+        leftPanels={leftPanels}
+        centerPanels={centerPanels}
+        rightPanels={rightPanels}
+      />
+    </ManifestEditorProvider>
   );
 }
