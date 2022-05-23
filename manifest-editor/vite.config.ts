cc2049c4944a93727f80f48860848add2943f9e5
Kelsie <kelsie.braidwood@digirati.com>
2022-05-23 14:00:07 +0100

    Add component names to classname

--- manifest-editor/vite.config.ts
+++ manifest-editor/vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        [
+          'babel-plugin-styled-components',
+          {
+            displayName: true,
+            fileName: false
+          }
+        ]
+      ]
+    }
+  })],
   test: {
     environment: "happy-dom", // or 'jsdom', 'node'
     globals: true,
