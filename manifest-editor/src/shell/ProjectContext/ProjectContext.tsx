b482ee76b86cce703676f1233f645e06c1e52d35
Stephen Fraser <stephen.fraser@digirati.com>
2022-05-24 01:36:34 +0100

    Update ProjectContext.tsx

--- manifest-editor/src/shell/ProjectContext/ProjectContext.tsx
+++ manifest-editor/src/shell/ProjectContext/ProjectContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useMemo, useReducer } from "react";
+import { createContext, ReactNode, useContext, useEffect, useMemo, useReducer } from "react";
 import { ProjectContext } from "./ProjectContext.types";
 import { useProjectActionsWithBackend, useProjectBackend, useProjectLoader } from "./ProjectContext.hooks";
 import { getDefaultProjectContextState, projectContextReducer } from "./ProjectContext.reducer";
@@ -7,6 +7,7 @@ import { ManifestContext, VaultProvider } from "react-iiif-vault";
 import { LocalStorageLoader } from "./storage/LocalStorageLoader";
 import { LocalStorageBackend } from "./backend/LocalStorageBackend";
 import { Loading } from "../../atoms/Loading";
+import { useShell } from "../../context/ShellContext/ShellContext";
 
 const ProjectReactContext = createContext<ProjectContext | null>(null);
 
@@ -15,7 +16,7 @@ export function ProjectProvider(props: { children: ReactNode }) {
   //   The interface for the loader will definitely change over time.
   const backend = useMemo(() => new LocalStorageBackend(), []);
   const storage = useMemo(() => new LocalStorageLoader(), []);
-
+  const shell = useShell();
   const [state, dispatch] = useReducer(projectContextReducer, undefined, getDefaultProjectContextState);
   const actions = useProjectActionsWithBackend(dispatch, backend);
   const context: ProjectContext = useMemo(() => ({ actions, ...state }), [actions, state]);
@@ -24,22 +25,27 @@ export function ProjectProvider(props: { children: ReactNode }) {
 
   useProjectBackend(context, backend);
 
+  useEffect(() => {
+    if (
+      state.loadingStatus &&
+      state.loadingStatus.loaded &&
+      ready &&
+      (!vault || !manifest) &&
+      shell.selectedApplication !== "Splash"
+    ) {
+      console.log("Changed to splash?");
+      shell.changeSelectedApplication("Splash");
+    }
+  }, []);
+
   if (state.loadingStatus && state.loadingStatus.loading) {
     return <div>Loading...</div>;
   }
 
-  if (!vault || !manifest || !ready) {
-    return (
-      <div>
-        <Loading />
-      </div>
-    );
-  }
-
   return (
     <ProjectReactContext.Provider value={context}>
-      <VaultProvider vault={vault} key={state.current?.id}>
-        <ManifestContext manifest={manifest.id}>{props.children}</ManifestContext>
+      <VaultProvider vault={vault || undefined} key={state.current?.id}>
+        <ManifestContext manifest={manifest?.id || ""}>{props.children}</ManifestContext>
       </VaultProvider>
     </ProjectReactContext.Provider>
   );
