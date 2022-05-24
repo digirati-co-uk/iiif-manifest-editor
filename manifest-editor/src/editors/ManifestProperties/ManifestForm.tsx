335bf59beb52350df2603ad96407ffe643392fff
Stephen Fraser <stephen.fraser@digirati.com>
2022-05-24 01:58:28 +0100

    Update ManifestForm.tsx

--- manifest-editor/src/editors/ManifestProperties/ManifestForm.tsx
+++ manifest-editor/src/editors/ManifestProperties/ManifestForm.tsx
@@ -7,41 +7,52 @@ import { MetadataForm } from "./MetadataForm";
 import { StructuralForm } from "./StructuralForm";
 import { TechnicalForm } from "./TechnicalForm";
 import { useLayoutProvider } from "../../shell/Layout/Layout.context";
+import { useOptionalResource } from "../../context/ResourceEditingContext/ResourceEditingContext";
+import { ErrorBoundary } from "react-error-boundary";
+import { useManifest } from "../../hooks/useManifest";
 
 export const ManifestForm: React.FC<{ current: number; setCurrent: (idx: number) => void }> = ({
   current = 0,
   setCurrent,
 }) => {
+  const manifest = useManifest();
+
+  if (!manifest) {
+    return null;
+  }
+
   return (
-    <TabPanel
-      menu={[
-        {
-          label: "Descriptive",
-          component: <DescriptiveForm />,
-        },
-        {
-          label: "Metadata",
-          component: <MetadataForm />,
-        },
-        {
-          label: "Linking",
-          component: <LinkingForm />,
-        },
-        {
-          label: "Technical",
-          component: <TechnicalForm />,
-        },
-        {
-          label: "Structural",
-          component: <StructuralForm />,
-        },
-        // {
-        //   label: "Annotations",
-        //   component: <div>I will be the annotations panel</div>,
-        // },
-      ]}
-      selected={current}
-      switchPanel={setCurrent}
-    />
+    <ErrorBoundary fallback={<div />} resetKeys={[manifest]}>
+      <TabPanel
+        menu={[
+          {
+            label: "Descriptive",
+            component: <DescriptiveForm />,
+          },
+          {
+            label: "Metadata",
+            component: <MetadataForm />,
+          },
+          {
+            label: "Linking",
+            component: <LinkingForm />,
+          },
+          {
+            label: "Technical",
+            component: <TechnicalForm />,
+          },
+          {
+            label: "Structural",
+            component: <StructuralForm />,
+          },
+          // {
+          //   label: "Annotations",
+          //   component: <div>I will be the annotations panel</div>,
+          // },
+        ]}
+        selected={current}
+        switchPanel={setCurrent}
+      />
+    </ErrorBoundary>
   );
 };
