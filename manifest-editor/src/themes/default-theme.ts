eae410e61762d9b068bd15b2e213f866bbc99a3b
Kelsie <kelsie.braidwood@digirati.com>
2022-05-25 17:57:02 +0100

    Language map styling and changes to functionality

--- manifest-editor/src/themes/default-theme.ts
+++ manifest-editor/src/themes/default-theme.ts
@@ -12,6 +12,7 @@ export const defaultTheme = {
     lightdanger: "#ffeaea",
     lightsuccess: "#c5e8c5",
     success: "#6ccd55",
+    highlight: "#faeed7"
   },
   iiifColor: {
     manifest: "rgba(0, 0, 0, 0.2)",
