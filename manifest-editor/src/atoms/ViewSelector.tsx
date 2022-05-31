b5eddb04983353d1b7400697080032565271653a
Kelsie <kelsie.braidwood@digirati.com>
2022-05-31 14:54:19 +0100

    Update ViewSelector to actions

--- manifest-editor/src/atoms/ViewSelector.tsx
+++ manifest-editor/src/atoms/ViewSelector.tsx
@@ -1,35 +1,42 @@
-import { useManifestEditor } from "../apps/ManifestEditor/ManifestEditor.context";
 import { GridIcon } from "../icons/GridIcon";
 import { ThumbnailStripIcon } from "../icons/ThumbnailStripIcon";
 import { TreeIcon } from "../icons/TreeIcon";
-import { FlexContainer } from "../components/layout/FlexContainer";
+import { FlexContainerRow } from "../components/layout/FlexContainer";
 import { Button } from "./Button";
+import { useLayoutActions } from "../shell/Layout/Layout.context";
 
 export const ViewSelector: React.FC = () => {
-  const editorContext = useManifestEditor();
+  const actions = useLayoutActions();
+
   return (
-    <FlexContainer>
+    <FlexContainerRow justify="center">
       <Button
-        onClick={() => editorContext?.setView("tree")}
+        onClick={() => actions.change("outline-view")}
         title="Switch to outline view"
         aria-label="Switch to outline view"
       >
         <TreeIcon />
       </Button>
       <Button
-        onClick={() => editorContext?.setView("grid")}
+        onClick={() => {
+          actions.centerPanel.change({ id: "thumbnail-grid" });
+          actions.leftPanel.close();
+        }}
         title="Switch to thumbnails only"
         aria-label="Switch to thumbnails only"
       >
         <GridIcon />
       </Button>
       <Button
-        onClick={() => editorContext?.setView("thumbnails")}
+        onClick={() => {
+          actions.change("thumbnail-view");
+          actions.change("current-canvas");
+        }}
         title="Switch to thumbnails with canvas"
         aria-label="Switch to thumbnails with canvas"
       >
         <ThumbnailStripIcon />
       </Button>
-    </FlexContainer>
+    </FlexContainerRow>
   );
 };
