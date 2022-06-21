b9e82071dc5464ad781d874c8398e845e934ca88
Stephen Fraser <stephen.fraser@digirati.com>
2022-06-21 11:12:49 +0100

    Fixed conflict

--- manifest-editor/src/shell/ProjectContext/ProjectContext.tsx
+++ manifest-editor/src/shell/ProjectContext/ProjectContext.tsx
@@ -1,23 +1,12 @@
 import { createContext, ReactNode, useContext, useEffect, useMemo, useReducer } from "react";
 import { ProjectContext } from "./ProjectContext.types";
-import {
-  useProjectActionsWithBackend,
-  useProjectBackend,
-  useProjectCreators,
-  useProjectLoader,
-} from "./ProjectContext.hooks";
+import { useProjectActionsWithBackend, useProjectBackend, useProjectLoader } from "./ProjectContext.hooks";
 import { getDefaultProjectContextState, projectContextReducer } from "./ProjectContext.reducer";
 import invariant from "tiny-invariant";
 import { ManifestContext, VaultProvider } from "react-iiif-vault";
 import { LocalStorageLoader } from "./storage/LocalStorageLoader";
 import { LocalStorageBackend } from "./backend/LocalStorageBackend";
 import { useApps } from "../AppContext/AppContext";
-<<<<<<< Updated upstream
-=======
-import { FileSystemFolderBackend } from "./backend/FileSystemFolderBackend";
-import { FileSystemLoader } from "./storage/FileSystemLoader";
-import { parse } from "query-string";
->>>>>>> Stashed changes
 
 const ProjectReactContext = createContext<ProjectContext | null>(null);
 
