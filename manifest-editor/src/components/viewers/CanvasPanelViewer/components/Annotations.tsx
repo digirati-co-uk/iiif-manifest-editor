8c29788f9c5e2dd3bcd3a53235fd1cd9eceeddac
Kelsie <kelsie.braidwood@digirati.com>
2022-06-28 17:33:59 +0100

    switch out worldobject for RegionHighlight

--- manifest-editor/src/components/viewers/CanvasPanelViewer/components/Annotations.tsx
+++ manifest-editor/src/components/viewers/CanvasPanelViewer/components/Annotations.tsx
@@ -1,5 +1,6 @@
 import { DrawBox, RegionHighlight, ResizeWorldItem, useControlledAnnotationList } from "@atlas-viewer/atlas";
-import { useAnnotation, useCanvas, useVault } from "react-iiif-vault";
+import { useState } from "react";
+import { useCanvas } from "react-iiif-vault";
 import { useAnnotationList } from "../../../../hooks/useAnnotationsList";
 
 function getAnnotationTarget(annotation: any) {
@@ -13,41 +14,64 @@ function getAnnotationTarget(annotation: any) {
   };
 }
 
-export function Annotation({ annotation }: { annotation: any }) {
-  return (
-    <world-object
-      x={0}
-      y={0}
-      width={getAnnotationTarget(annotation).width}
-      height={getAnnotationTarget(annotation).height}
-    >
-      <box
-        interactive
-        // onClick={(e) => {
-        //   e.preventDefault();
-        //   e.stopPropagation();
-        //   // onClick(region);
-        // }}
-        target={{
-          x: getAnnotationTarget(annotation).x,
-          y: getAnnotationTarget(annotation).y,
-          width: getAnnotationTarget(annotation).width,
-          height: getAnnotationTarget(annotation).height,
-        }}
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.4" }}
-      />
-    </world-object>
-  );
-}
+// export function Annotation({ annotation }: { annotation: any }) {
+//   return (
+//     <world-object
+//       x={0}
+//       y={0}
+//       width={getAnnotationTarget(annotation).width}
+//       height={getAnnotationTarget(annotation).height}
+//     >
+//       <box
+//         interactive
+//         // onClick={(e) => {
+//         //   e.preventDefault();
+//         //   e.stopPropagation();
+//         //   // onClick(region);
+//         // }}
+//         target={{
+//           x: getAnnotationTarget(annotation).x,
+//           y: getAnnotationTarget(annotation).y,
+//           width: getAnnotationTarget(annotation).width,
+//           height: getAnnotationTarget(annotation).height,
+//         }}
+//         style={{ backgroundColor: "rgba(0, 0, 0, 0.4" }}
+//       />
+//     </world-object>
+//   );
+// }
 
 export function Annotations({ canvasId }: { canvasId: string }) {
   const annotations = useAnnotationList(canvasId);
   const canvas = useCanvas({ id: canvasId }) as any;
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [selectedAnnotation, setSelectedAnnotation] = useState<string>();
   return (
     <world-object height={canvas.height} width={canvas.width}>
+      {isEditing && !selectedAnnotation ? <DrawBox onCreate={() => console.log("createNewFunctionNeeded")} /> : null}
       {annotations.map((annotation: any) => {
         if (annotation.target) {
-          return <Annotation annotation={annotation} />;
+          return (
+            <RegionHighlight
+              interactive
+              key={annotation.id}
+              region={getAnnotationTarget(annotation)}
+              isEditing={selectedAnnotation === annotation.id && isEditing}
+              onSave={() => {
+                // on save function goes here
+              }}
+              onClick={() => {
+                console.log(annotation);
+                setIsEditing(true);
+                setSelectedAnnotation(annotation.id);
+              }}
+              style={
+                selectedAnnotation === annotation.id && isEditing
+                  ? { border: " 5px solid green" }
+                  : { backgroundColor: " rgba(0, 0, 0, 0.4)" }
+              }
+            />
+          );
         }
       })}
     </world-object>
