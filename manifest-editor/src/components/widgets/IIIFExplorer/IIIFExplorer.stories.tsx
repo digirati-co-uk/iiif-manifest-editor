d0849a9059b3b8b5900748fddbfff9d2ab396590
Stephen Fraser <stephen.fraser@digirati.com>
2023-02-01 17:05:40 +0000

    Update IIIFExplorer.stories.tsx

--- manifest-editor/src/components/widgets/IIIFExplorer/IIIFExplorer.stories.tsx
+++ manifest-editor/src/components/widgets/IIIFExplorer/IIIFExplorer.stories.tsx
@@ -3,7 +3,7 @@ import { IIIFExplorer } from "@/components/widgets/IIIFExplorer/IIIFExplorer";
 
 export default { title: "IIIF Explorer" };
 
-// export const Default = () => <IIIFExplorer />;
+export const Default = () => <IIIFExplorer />;
 
 export const TopLevelCollection = () => (
   <IIIFExplorer entry={{ id: "https://view.nls.uk/collections/top.json", type: "Collection" }} />
