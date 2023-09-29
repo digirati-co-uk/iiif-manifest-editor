531b74902876c543a889d74adf59d3dda7bb0601
Stephen Fraser <stephen.fraser@digirati.com>
2023-09-29 14:11:40 +0100

    Fixed typo

--- manifest-editor/src/components/widgets/IIIFExplorer/components/ExplorerInput.tsx
+++ manifest-editor/src/components/widgets/IIIFExplorer/components/ExplorerInput.tsx
@@ -20,7 +20,13 @@ export function ExplorerInput() {
 
   return (
     <form onSubmit={onSubmit} className={$.InputContainer}>
-      <input type="text" placeholder="Enter Manifest or Collection URL..." className={$.Input} name="url" id="url" />
+      <input
+        type="text"
+        placeholder="Enter IIIF Manifest or Collection URL..."
+        className={$.Input}
+        name="url"
+        id="url"
+      />
     </form>
   );
 }
