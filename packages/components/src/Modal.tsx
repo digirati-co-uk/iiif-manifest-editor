f5f4331a8741cfc2590c5c37713720c898bef1fa
Stephen Fraser <stephen.fraser@digirati.com>
2024-06-01 12:44:59 +0100

    Revert modal positioning

--- packages/components/src/Modal.tsx
+++ packages/components/src/Modal.tsx
@@ -17,7 +17,7 @@ export function Modal({ id, title, open = true, onClose, actions, children }: Mo
       <Dialog open={open} onClose={onClose} id={id} className="relative z-[500]">
         <div className={`fixed inset-0 bg-black/30 animate-fadeIn z-[501]`} aria-hidden="true" />
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4 z-[502]">
-          <div className="relative p-4 w-full max-w-4xl max-h-full mb-[25%]">
+          <div className="relative p-4 w-full max-w-4xl max-h-full">
             <DialogPanel className="relative bg-white rounded-lg overflow-hidden shadow-2xl dark:bg-gray-700 max-h-[80vh] flex flex-col">
               <div className="flex  items-center justify-between p-4 md:p-5 rounded-t-lg dark:border-gray-600 sticky top-0 bg-white">
                 <DialogTitle className="text-xl font-semibold text-gray-900 dark:text-white">{title}</DialogTitle>
