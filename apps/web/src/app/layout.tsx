44e7b4163936fd8de0c37f0f532ecf7328557370
Stephen Fraser <stephen.fraser@digirati.com>
2024-06-11 10:52:22 +0100

    Re-added icons

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,6 +15,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={inter.className}>
         <SiteProvider>{children}</SiteProvider>
       </body>
