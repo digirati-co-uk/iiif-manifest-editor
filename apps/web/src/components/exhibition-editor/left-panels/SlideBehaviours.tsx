8269b257ba3fb30a37fb2545d4935cf4480a724b
Stephen Fraser <stephen.fraser@digirati.com>
2025-01-29 17:23:37 +0000

    Added behaviour editing for layout

--- apps/web/src/components/exhibition-editor/left-panels/SlideBehaviours.tsx
+++ apps/web/src/components/exhibition-editor/left-panels/SlideBehaviours.tsx
@@ -1,9 +1,12 @@
+import { Sidebar, SidebarHeader } from "@manifest-editor/components";
+import { behaviors } from "@manifest-editor/editor-api";
 import { useInStack, BehaviorEditor, BehaviorEditorProps } from "@manifest-editor/editors";
 import { LayoutPanel, useEditor } from "@manifest-editor/shell";
+import { useState } from "react";
 
 export const slideBehaviours: LayoutPanel = {
-  id: 'slide-behaviours',
-  label: 'Slide behaviours',
+  id: 'slide-behaviors',
+  label: 'Slide behaviors',
   icon: <>🐙</>,
   render: () => <SlideBehavioursPanel />,
 };
@@ -33,25 +36,111 @@ const exhibitionConfigs: BehaviorEditorProps['configs'] = [
         value: 'image',
       }
     ],
+  },
+  {
+    id: 'size',
+    component: (existing, setBehaviors) => <EditSize behaviors={existing} setBehaviors={setBehaviors} />,
+    label: { en: ['Size'] },
+    type: 'custom',
+    supports: (b) => b.startsWith('w-') || b.startsWith('h-'),
   }
 ];
+function parseBehaviors(items: string[]) {
+  let width = 0;
+  let height = 0;
+
+  for (const item of items) {
+    if (item.startsWith('w-')) {
+      width = parseInt(item.slice(2));
+    }
+    if (item.startsWith('h-')) {
+      height = parseInt(item.slice(2));
+    }
+  }
+
+
+  return {
+    width,
+    height,
+  }
+}
+
+function EditSize({ behaviors, setBehaviors }: { behaviors: string[]; setBehaviors: (behaviors: string[]) => void }) {
+  const { width, height } = parseBehaviors(behaviors)
+  const [hoverPosition, setHoverPosition] = useState({ x: -1, y: -1 });
+
+
+  const setBehavior = (w: number, h: number) => {
+    const newBehaviors = behaviors.filter(b => {
+      return !b.startsWith('w-') && !b.startsWith('h-');
+    })
+
+    newBehaviors.push(`w-${w}`);
+    newBehaviors.push(`h-${h}`);
+
+    setBehaviors(newBehaviors);
+  }
+
+  const cells = Array.from({ length: 144 }, (_, i) => {
+    const x = i % 12;
+    const y = Math.floor(i / 12);
+    const hovered = hoverPosition.x >= x && hoverPosition.y >= y;
+    const selected = x < width && y < height;
+    const hoveredAndSelected = hovered && selected;
+
+    let bgClass = 'bg-gray-200';
+    if (hovered) {
+      bgClass = 'bg-me-200';
+    }
+    if (selected) {
+      bgClass = 'bg-me-500';
+    }
+    if (hoveredAndSelected) {
+      bgClass = 'bg-me-400';
+    }
+
+    return (
+      <div
+        key={i}
+        className={
+          `flex aspect-square rounded cursor-se-resize items-center justify-center ${bgClass}`
+        }
+        onClick={() => setBehavior(x + 1, y + 1)}
+        onMouseUp={() => setBehavior(x + 1, y + 1)}
+        onMouseEnter={() => setHoverPosition({ x, y })}
+      />
+    );
+  });
+
+  return (
+    <div className="grid grid-cols-12 grid-rows-12 gap-1" onMouseLeave={() => setHoverPosition({ x: -1, y: -1 })}>
+      {cells}
+    </div>
+  );
+}
 
 function SlideBehavioursPanel() {
   const canvas = useInStack("Canvas");
   const editor = useEditor();
 
   if (!canvas || editor.technical.type !== "Canvas") {
-    return <div>Please select canvas</div>;
+    return (
+      <Sidebar>
+        <SidebarHeader title="Slide behaviors" />
+        <div>Please select canvas</div>
+      </Sidebar>
+    );
   }
 
 
   return (
-    <div>Canvas!
+    <Sidebar>
+      <SidebarHeader title="Slide behaviors" />
       <BehaviorEditor
         behavior={editor.technical.behavior.get()}
         onChange={(v) => editor.technical.behavior.set(v)}
         configs={exhibitionConfigs}
       />
-    </div>
+    </Sidebar>
   );
 }
