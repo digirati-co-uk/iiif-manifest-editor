0729e39f683bac60482b28794010340046cc5332
Stephen Fraser <stephen.fraser@digirati.com>
2025-02-04 08:06:15 +0000

    Linting

--- apps/web/src/components/exhibition-editor/left-panels/SlideBehaviours.tsx
+++ apps/web/src/components/exhibition-editor/left-panels/SlideBehaviours.tsx
@@ -1,11 +1,19 @@
 import { Sidebar, SidebarHeader } from "@manifest-editor/components";
-import { useInStack, BehaviorEditor, BehaviorEditorProps } from "@manifest-editor/editors";
-import { EditorDefinition, LayoutPanel, useEditor } from "@manifest-editor/shell";
+import {
+  BehaviorEditor,
+  type BehaviorEditorProps,
+  useInStack,
+} from "@manifest-editor/editors";
+import {
+  type EditorDefinition,
+  type LayoutPanel,
+  useEditor,
+} from "@manifest-editor/shell";
 import { useState } from "react";
 
 export const slideBehaviours: LayoutPanel = {
-  id: 'slide-behaviors',
-  label: 'Slide behaviors',
+  id: "slide-behaviors",
+  label: "Slide behaviors",
   icon: <>🐙</>,
   render: () => <SlideBehavioursPanel />,
 };
@@ -15,86 +23,87 @@ export const customBehaviourEditor: EditorDefinition = {
   supports: {
     edit: true,
     resourceTypes: ["Canvas"],
-    properties: ["behavior"]
+    properties: ["behavior"],
   },
-  id: 'slide-behaviors',
-  label: 'Slide behaviors',
+  id: "slide-behaviors",
+  label: "Slide behaviors",
   tabs: {
     showTitle: true,
-  }
+  },
 };
 
-const exhibitionConfigs: BehaviorEditorProps['configs'] = [
+const exhibitionConfigs: BehaviorEditorProps["configs"] = [
   {
-    id: 'layout',
-    type: 'choice',
-    label: { en: ['Layout'] },
+    id: "layout",
+    type: "choice",
+    label: { en: ["Layout"] },
     initialOpen: true,
     items: [
       {
-        label: { en: ['Text on left'] },
-        value: 'left',
-
+        label: { en: ["Text on left"] },
+        value: "left",
       },
       {
-        label: { en: ['Text on right'] },
-        value: 'right',
-
-      }, {
-        label: { en: ['Text on bottom'] },
-        value: 'bottom',
-
+        label: { en: ["Text on right"] },
+        value: "right",
       },
       {
-        label: { en: ['Only image'] },
-        value: 'image',
-      }
+        label: { en: ["Text on bottom"] },
+        value: "bottom",
+      },
+      {
+        label: { en: ["Only image"] },
+        value: "image",
+      },
     ],
   },
   {
-    id: 'size',
-    component: (existing, setBehaviors) => <EditSize behaviors={existing} setBehaviors={setBehaviors} />,
-    label: { en: ['Size'] },
-    type: 'custom',
+    id: "size",
+    component: (existing, setBehaviors) => (
+      <EditSize behaviors={existing} setBehaviors={setBehaviors} />
+    ),
+    label: { en: ["Size"] },
+    type: "custom",
     initialOpen: true,
-    supports: (b) => b.startsWith('w-') || b.startsWith('h-'),
-  }
+    supports: (b) => b.startsWith("w-") || b.startsWith("h-"),
+  },
 ];
 function parseBehaviors(items: string[]) {
   let width = 0;
   let height = 0;
 
   for (const item of items) {
-    if (item.startsWith('w-')) {
-      width = parseInt(item.slice(2));
+    if (item.startsWith("w-")) {
+      width = Number.parseInt(item.slice(2));
     }
-    if (item.startsWith('h-')) {
-      height = parseInt(item.slice(2));
+    if (item.startsWith("h-")) {
+      height = Number.parseInt(item.slice(2));
     }
   }
-
 
   return {
     width,
     height,
-  }
+  };
 }
 
-function EditSize({ behaviors, setBehaviors }: { behaviors: string[]; setBehaviors: (behaviors: string[]) => void }) {
-  const { width, height } = parseBehaviors(behaviors)
+function EditSize({
+  behaviors,
+  setBehaviors,
+}: { behaviors: string[]; setBehaviors: (behaviors: string[]) => void }) {
+  const { width, height } = parseBehaviors(behaviors);
   const [hoverPosition, setHoverPosition] = useState({ x: -1, y: -1 });
 
-
   const setBehavior = (w: number, h: number) => {
-    const newBehaviors = behaviors.filter(b => {
-      return !b.startsWith('w-') && !b.startsWith('h-');
-    })
+    const newBehaviors = behaviors.filter((b) => {
+      return !b.startsWith("w-") && !b.startsWith("h-");
+    });
 
     newBehaviors.push(`w-${w}`);
     newBehaviors.push(`h-${h}`);
 
     setBehaviors(newBehaviors);
-  }
+  };
 
   const cells = Array.from({ length: 144 }, (_, i) => {
     const x = i % 12;
@@ -103,23 +112,21 @@ function EditSize({ behaviors, setBehaviors }: { behaviors: string[]; setBehavio
     const selected = x < width && y < height;
     const hoveredAndSelected = hovered && selected;
 
-    let bgClass = 'bg-gray-200';
+    let bgClass = "bg-gray-200";
     if (hovered) {
-      bgClass = 'bg-me-200';
+      bgClass = "bg-me-200";
     }
     if (selected) {
-      bgClass = 'bg-me-500';
+      bgClass = "bg-me-500";
     }
     if (hoveredAndSelected) {
-      bgClass = 'bg-me-400';
+      bgClass = "bg-me-400";
     }
 
     return (
       <div
         key={i}
-        className={
-          `flex aspect-square rounded cursor-se-resize items-center justify-center ${bgClass}`
-        }
+        className={`flex aspect-square rounded cursor-se-resize items-center justify-center ${bgClass}`}
         onClick={() => setBehavior(x + 1, y + 1)}
         onMouseUp={() => setBehavior(x + 1, y + 1)}
         onMouseEnter={() => setHoverPosition({ x, y })}
@@ -128,7 +135,10 @@ function EditSize({ behaviors, setBehaviors }: { behaviors: string[]; setBehavio
   });
 
   return (
-    <div className="grid grid-cols-12 grid-rows-12 gap-1" onMouseLeave={() => setHoverPosition({ x: -1, y: -1 })}>
+    <div
+      className="grid grid-cols-12 grid-rows-12 gap-1"
+      onMouseLeave={() => setHoverPosition({ x: -1, y: -1 })}
+    >
       {cells}
     </div>
   );
@@ -146,7 +156,6 @@ function SlideBehavioursPanel() {
       </Sidebar>
     );
   }
-
 
   return (
     <Sidebar>
