60c1c07e49c6e65b7e35db65aa26aba56fab50ea
Stephen Fraser <stephen.fraser@digirati.com>
2025-02-12 17:13:52 +0000

    Hotfix for netlify preview (main)

--- packages/iiif-preview-server/src/stores/netlify-blob.ts
+++ packages/iiif-preview-server/src/stores/netlify-blob.ts
@@ -1,26 +1,27 @@
 import { getStore } from "@netlify/blobs";
-import { StorageInterface } from "../types";
+import type { StorageInterface } from "../types";
 
 export function createNetlifyStore(
   config: {
     defaultTtl?: number;
     netlifyStoreName?: string;
-  } = {}
+  } = {},
 ): StorageInterface {
   const manifests = getStore(config.netlifyStoreName || "preview-manifests");
 
   return {
     get: async (key: string) => {
-      const itemString = await manifests.get(key);
-      const item = itemString ? JSON.parse(itemString) : null;
+      const resource = await manifests.getWithMetadata(key);
+      const metadata = (resource?.metadata || {}) as any;
+      const item = resource?.data ? JSON.parse(resource.data) : null;
 
       if (item) {
-        if (item.metadata.ttl < Date.now()) {
+        if (metadata?.ttl < Date.now()) {
           manifests.delete(key);
           return null;
         }
 
-        return item.value;
+        return item;
       }
       return null;
     },
@@ -34,7 +35,7 @@ export function createNetlifyStore(
       const item = await manifests.getWithMetadata(key);
       if (item) {
         const metadata = item.metadata as { ttl?: number };
-        if (metadata.ttl && metadata.ttl < Date.now()) {
+        if (metadata?.ttl && metadata?.ttl < Date.now()) {
           await manifests.delete(key);
           return { value: null, metadata: null };
         }
@@ -42,13 +43,18 @@ export function createNetlifyStore(
       }
       return { value: null, metadata: null };
     },
-    put: async (key: string, value: any, options: { expirationTtl?: number; metadata?: { ttl?: number } }) => {
+    put: async (
+      key: string,
+      value: any,
+      options: { expirationTtl?: number; metadata?: { ttl?: number } },
+    ) => {
       options.metadata = options.metadata || {};
       if (options.expirationTtl && !options.metadata.ttl) {
         options.metadata.ttl = Date.now() + options.expirationTtl;
       }
       if (!options.metadata.ttl) {
-        options.metadata.ttl = config.defaultTtl || Date.now() + 1000 * 60 * 60 * 24 * 2; // 48 hours
+        options.metadata.ttl =
+          config.defaultTtl || Date.now() + 1000 * 60 * 60 * 24 * 2; // 48 hours
       }
       await manifests.set(key, JSON.stringify(value), options);
     },
