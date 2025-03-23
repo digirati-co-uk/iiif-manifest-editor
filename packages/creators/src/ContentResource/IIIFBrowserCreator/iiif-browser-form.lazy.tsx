3d9c08ea694ae79dd3141292bcb984c6fda61a9d
Stephen Fraser <stephen.fraser@digirati.com>
2025-03-23 17:29:12 +0000

    Fixes for Browser and new bug fix version

--- packages/creators/src/ContentResource/IIIFBrowserCreator/iiif-browser-form.lazy.tsx
+++ packages/creators/src/ContentResource/IIIFBrowserCreator/iiif-browser-form.lazy.tsx
@@ -1,3 +1,4 @@
+import { Vault } from "@iiif/helpers";
 import type { CreatorContext } from "@manifest-editor/creator-api";
 import { IIIFExplorer } from "@manifest-editor/iiif-browser";
 import {
@@ -10,7 +11,7 @@ import { IIIFBrowser, type IIIFBrowserProps } from "iiif-browser";
 import { useMemo } from "react";
 
 export default function IIIFBrowserCreatorForm(props: CreatorContext) {
-  const vault = usePreviewVault();
+  const vault = useMemo(() => new Vault(), []);
   const output = useMemo(() => {
     return [
       {
@@ -29,20 +30,25 @@ export default function IIIFBrowserCreatorForm(props: CreatorContext) {
     ] as IIIFBrowserProps["output"];
   }, [props.runCreate]);
 
+  const navigationOptions = useMemo(() => {
+    return {
+      canCropImage: true,
+      canSelectCanvas: true,
+      canSelectManifest: false,
+      canSelectCollection: false,
+      // @todo fix the output so this one works.
+      multiSelect: false,
+    };
+  }, []);
+
   return (
     <PreviewVaultBoundary>
       <IIIFBrowser
+        debug
         vault={vault}
         className="iiif-browser border-none border-t rounded-none h-[70vh] min-h-[60vh] max-h-full max-w-full"
         output={output}
-        navigation={{
-          canCropImage: true,
-          canSelectCanvas: true,
-          canSelectManifest: false,
-          canSelectCollection: false,
-          // @todo fix the output so this one works.
-          multiSelect: false,
-        }}
+        navigation={navigationOptions}
       />
     </PreviewVaultBoundary>
   );
