bd8d393b5af8b62a521499581bf5992eb0b0ae22
Stephen Fraser <stephen.fraser@digirati.com>
2025-04-16 11:04:46 +0100

    YouTube larger thumbnail

--- presets/exhibition-preset/src/canvas-editors/youtube-editor.tsx
+++ presets/exhibition-preset/src/canvas-editors/youtube-editor.tsx
@@ -1,9 +1,5 @@
 import { ActionButton, LazyThumbnail } from "@manifest-editor/components";
-import {
-  type CanvasEditorDefinition,
-  useGenericEditor,
-  useLayoutActions,
-} from "@manifest-editor/shell";
+import { type CanvasEditorDefinition, useGenericEditor, useLayoutActions } from "@manifest-editor/shell";
 import { useRef } from "react";
 import {
   LocaleString,
@@ -19,10 +15,7 @@ export const youtubeMainEdtior: CanvasEditorDefinition = {
   label: "YouTube main editor",
   component: (strategy) =>
     strategy.type === "media" ? (
-      <YouTubeEditor
-        strategy={strategy as MediaStrategy}
-        video={strategy.media as SingleYouTubeVideo}
-      />
+      <YouTubeEditor strategy={strategy as MediaStrategy} video={strategy.media as SingleYouTubeVideo} />
     ) : null,
   supports: {
     strategy: (strategy, resource, vault) => {
@@ -40,10 +33,7 @@ export const youtubeMainEdtior: CanvasEditorDefinition = {
   },
 };
 
-export function YouTubeEditor({
-  strategy,
-  video,
-}: { strategy: MediaStrategy; video: SingleYouTubeVideo }) {
+export function YouTubeEditor({ strategy, video }: { strategy: MediaStrategy; video: SingleYouTubeVideo }) {
   const { edit } = useLayoutActions();
   const canvas = useCanvas();
   const annotationRef = useVaultSelector((_, v) => {
@@ -56,15 +46,12 @@ export function YouTubeEditor({
   const media = video;
 
   const ar = (canvas.width || 1) / (canvas.height || 1);
-  const ytThumbnail = `https://img.youtube.com/vi/${media.youTubeId}/0.jpg`;
+  const ytThumbnail = `https://img.youtube.com/vi/${media.youTubeId}/maxresdefault.jpg`;
 
   return (
     <div className="overflow-y-auto">
       <div className="p-8">
-        <div
-          className="bg-black z-20 flex p-8 rounded-lg max-w-full max-h-[70vh] mx-auto"
-          style={{ aspectRatio: ar }}
-        >
+        <div className="bg-black z-20 flex p-8 rounded-lg max-w-full max-h-[70vh] mx-auto" style={{ aspectRatio: ar }}>
           <iframe
             title="YouTube video"
             className="border-none w-full h-full object-contain"
@@ -85,9 +72,7 @@ export function YouTubeEditor({
             {canvas.thumbnail?.length ? (
               <LazyThumbnail cover />
             ) : (
-              <div className="text-white/50 text-center py-16">
-                No thumbnail
-              </div>
+              <div className="text-white/50 text-center py-16">No thumbnail</div>
             )}
           </div>
         </div>
@@ -95,24 +80,18 @@ export function YouTubeEditor({
           <LocaleString
             as="h3"
             className="text-2xl font-bold mt-4"
-            defaultText={
-              (<span className="text-black/30">No Label</span>) as any
-            }
+            defaultText={(<span className="text-black/30">No Label</span>) as any}
           >
             {annotation.descriptive.label.get()}
           </LocaleString>
           <LocaleString
             as="p"
             className="mt-2 mb-8"
-            defaultText={
-              (<span className="text-black/30">No summary</span>) as any
-            }
+            defaultText={(<span className="text-black/30">No summary</span>) as any}
           >
             {annotation.descriptive.summary.get()}
           </LocaleString>
-          <ActionButton onPress={() => edit(annotation.ref())}>
-            Edit properties
-          </ActionButton>
+          <ActionButton onPress={() => edit(annotation.ref())}>Edit properties</ActionButton>
         </div>
       </div>
     </div>
