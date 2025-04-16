18017f7def8047d180657bb08ed52e80c7954ee2
Stephen Fraser <stephen.fraser@digirati.com>
2025-04-16 17:13:55 +0100

    Switched manifest browser to use new creator

--- packages/creators/src/Manifest/ManifestBrowserCreator/manifest-browser-form.lazy.tsx
+++ packages/creators/src/Manifest/ManifestBrowserCreator/manifest-browser-form.lazy.tsx
@@ -1,38 +1,50 @@
+import { Vault } from "@iiif/helpers";
 import type { CreatorContext } from "@manifest-editor/creator-api";
-import { IIIFExplorer } from "@manifest-editor/iiif-browser";
-import {
-  usePreviewVault,
-  usePreviewHistory,
-  PreviewVaultBoundary,
-  HOMEPAGE_COLLECTION,
-} from "@manifest-editor/shell";
+import { PreviewVaultBoundary } from "@manifest-editor/shell";
+import { IIIFBrowser, type IIIFBrowserProps } from "iiif-browser";
+import { useMemo } from "react";
 
 export default function ManifestBrowserCreatorForm(props: CreatorContext) {
-  const vault = usePreviewVault();
-  const { addHistory, clearHistory } = usePreviewHistory();
+  const vault = useMemo(() => new Vault(), []);
+  const output = useMemo(() => {
+    return [
+      {
+        type: "callback",
+        label: "Select",
+        supportedTypes: ["Manifest", "Collection"],
+        cb: (resource) => props.runCreate({ output: resource }),
+        format: { type: "content-state" },
+      },
+    ] as IIIFBrowserProps["output"];
+  }, [props.runCreate]);
+
+  const navigationOptions = useMemo(() => {
+    return {
+      canCropImage: false,
+      canSelectCanvas: false,
+      canSelectManifest: true,
+      canSelectCollection: true,
+      // @todo fix the output so this one works.
+      multiSelect: false,
+    };
+  }, []);
+
+  const uiOptions = useMemo(() => {
+    return {
+      buttonClassName: "bg-me-primary-500 text-white hover:bg-me-primary-600",
+    } as IIIFBrowserProps["ui"];
+  }, []);
 
   return (
-    <>
-      <PreviewVaultBoundary>
-        <IIIFExplorer
-          window={false}
-          hideHeader={true}
-          outputTypes={["Manifest", "Collection"]}
-          vault={vault}
-          output={{ type: "content-state" }}
-          homepageCollection={HOMEPAGE_COLLECTION}
-          clearHomepageCollection={clearHistory}
-          onHistory={addHistory}
-          outputTargets={[
-            {
-              type: "callback",
-              label: "Select",
-              cb: (resource) => props.runCreate({ output: resource }),
-            },
-          ]}
-          allowRemoveEntry
-        />
-      </PreviewVaultBoundary>
-    </>
+    <PreviewVaultBoundary>
+      <IIIFBrowser
+        debug
+        ui={uiOptions}
+        vault={vault}
+        className="iiif-browser border-none border-t rounded-none h-[70vh] min-h-[60vh] max-h-full max-w-full"
+        output={output}
+        navigation={navigationOptions}
+      />
+    </PreviewVaultBoundary>
   );
 }
