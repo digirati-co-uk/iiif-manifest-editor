4487422434966a326c7ba330c8c12589ba3148b9
Stephen Fraser <stephen.fraser@digirati.com>
2025-05-06 12:54:40 +0100

    Missing label on collection items

--- packages/components/src/CollectionListItem.tsx
+++ packages/components/src/CollectionListItem.tsx
@@ -1,6 +1,7 @@
 import { LocaleString, useCollection } from "react-iiif-vault";
 import { CollectionIcon } from "./icons/CollectionIcon";
 import { usePress } from "react-aria";
+import { getValue } from "@iiif/helpers/i18n";
 
 export function CollectionListItem({ isActive, onAction }: { isActive: boolean; onAction: () => void }) {
   const collection = useCollection({} as any);
@@ -10,6 +11,7 @@ export function CollectionListItem({ isActive, onAction }: { isActive: boolean; 
 
   return (
     <li
+      aria-label={getValue(collection?.label)}
       className="border-b-2 border-b-gray-200 flex p-2 gap-3 text-left truncate items-center w-full hover:bg-gray-100 data-[active]:bg-gray-200 data-[active]:border-b-me-primary-500"
       data-active={isActive || undefined}
       {...pressProps}
