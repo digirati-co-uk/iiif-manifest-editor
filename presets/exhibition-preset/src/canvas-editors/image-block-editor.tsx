4da00c40a7c1112b6a90f9729ad7efd738a15a5c
Stephen Fraser <stephen.fraser@digirati.com>
2025-06-24 16:26:35 +0100

    Scale preview for layout improvement

--- presets/exhibition-preset/src/canvas-editors/image-block-editor.tsx
+++ presets/exhibition-preset/src/canvas-editors/image-block-editor.tsx
@@ -1,23 +1,12 @@
 import { ActionButton } from "@manifest-editor/components";
-import {
-  CanvasPanelEditor,
-  useInStack,
-  useToggleList,
-} from "@manifest-editor/editors";
-import type {
-  CanvasEditorDefinition,
-  LayoutPanel,
-} from "@manifest-editor/shell";
+import { CanvasPanelEditor, useInStack, useToggleList } from "@manifest-editor/editors";
+import type { CanvasEditorDefinition, LayoutPanel } from "@manifest-editor/shell";
 import { useLocalStorage } from "@manifest-editor/ui/madoc/use-local-storage";
 import { Button } from "react-aria-components";
-import {
-  CanvasContext,
-  LocaleString,
-  type RenderingStrategy,
-  useCanvas,
-} from "react-iiif-vault";
+import { CanvasContext, LocaleString, type RenderingStrategy, useCanvas } from "react-iiif-vault";
 import { twMerge } from "tailwind-merge";
 import { getClassName, getGridStats } from "../helpers";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export const imageBlockEditor: CanvasEditorDefinition = {
   id: "image-block-editor",
@@ -38,11 +27,7 @@ export const imageBlockEditor: CanvasEditorDefinition = {
 
       let isImage = false;
       for (const behaviour of behaviors) {
-        if (
-          behaviour.startsWith("w-") ||
-          behaviour.startsWith("h-") ||
-          behaviour === "image"
-        ) {
+        if (behaviour.startsWith("w-") || behaviour.startsWith("h-") || behaviour === "image") {
           isImage = true;
         }
       }
@@ -52,9 +37,7 @@ export const imageBlockEditor: CanvasEditorDefinition = {
   },
 };
 
-export function ImageBlockEditor({
-  strategy,
-}: { strategy: RenderingStrategy }) {
+export function ImageBlockEditor({ strategy }: { strategy: RenderingStrategy }) {
   const [isPreview, setIsPreview] = useLocalStorage("exhibition-preview-mode");
   const canvas = useInStack("Canvas");
   if (!canvas) {
@@ -70,9 +53,7 @@ export function ImageBlockEditor({
         </ActionButton>
       </div>
       {isPreview ? (
-        <div className="grid grid-cols-12 grid-rows-12 h-full bg-white">
-          <ImageBlockRenderer />
-        </div>
+        <ImageBlockRenderer />
       ) : (
         <div className="h-full flex flex-1 min-h-0">
           <CanvasPanelEditor asFallback />
@@ -83,30 +64,48 @@ export function ImageBlockEditor({
 }
 
 function ImageBlockRenderer() {
+  const ref = useRef<HTMLDivElement>(null);
   const canvas = useCanvas();
   const behavior = canvas?.behavior || [];
-  const className = getClassName(behavior);
+  const className = getClassName(behavior, false, false);
   const { isBottom, isImage, isInfo, isLeft } = getGridStats(canvas?.behavior);
 
+  const [scale, setScale] = useState(1);
+
+  useLayoutEffect(() => {
+    // 1200 is what it looks like on the site.
+    const container = ref.current;
+    if (!container) return;
+    const containerWidth = container.offsetWidth;
+    const imageWidth = 1200;
+    setScale(containerWidth / imageWidth);
+  }, [canvas]);
+
   return (
-    <div
-      className={twMerge(
-        className,
-        "flex h-full max-h-full min-h-0",
-        isLeft ? "flex-row-reverse" : isBottom ? "flex-col" : "flex-row",
-      )}
-    >
-      <div className="flex-1 h-full flex min-w-0">
-        <CanvasPanelEditor asFallback />
-      </div>
-      {!isImage ? (
+    <div className="overflow-hidden h-full w-full" ref={ref}>
+      <div className="grid grid-cols-12 grid-rows-12 h-full bg-white">
         <div
-          className={`${isBottom ? "min-h-3 w-full" : "w-1/3"} flex-shrink-0 self-stretch text-white bg-black rounded p-8`}
+          className={twMerge(
+            className,
+            "flex h-full max-h-full min-h-0 ring-4 ring-black",
+            isLeft ? "flex-row-reverse" : isBottom ? "flex-col" : "flex-row"
+          )}
         >
-          <LocaleString className="block mb-4">{canvas?.label}</LocaleString>
-          <LocaleString>{canvas?.summary}</LocaleString>
+          <div className="flex-1 h-full flex min-w-0">
+            <CanvasPanelEditor asFallback />
+          </div>
+          {!isImage ? (
+            <div
+              className={`${isBottom ? "min-h-3 w-full" : "w-1/3"} flex-shrink-0 self-stretch text-white bg-black rounded p-8 relative h-full`}
+            >
+              <div style={{ fontSize: `${scale * 16}px` }}>
+                <LocaleString className="block mb-4">{canvas?.label}</LocaleString>
+                <LocaleString>{canvas?.summary}</LocaleString>
+              </div>
+            </div>
+          ) : null}
         </div>
-      ) : null}
+      </div>
     </div>
   );
 }
