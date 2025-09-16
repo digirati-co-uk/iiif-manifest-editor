b1281294ac898ef431dfb4550e3ad73eb8a1acf9
Heather0K <hkerri200@gmail.com>
2025-09-16 14:23:49 +0100

    add info text for creating new range

--- presets/manifest-preset/src/left-panels/components/RangeSplittingPreview.tsx
+++ presets/manifest-preset/src/left-panels/components/RangeSplittingPreview.tsx
@@ -1,6 +1,7 @@
 import { ActionButton, SidebarContent } from "@manifest-editor/components";
 import { useRangeSplittingStore } from "../../store/range-splitting-store";
 import { SplitRangeIcon } from "../../icons";
+import { InfoIcon } from "@manifest-editor/ui/icons/InfoIcon";
 
 export function RangeSplittingPreview() {
   const { setIsSplitting } = useRangeSplittingStore();
@@ -15,6 +16,12 @@ export function RangeSplittingPreview() {
         <div className="text-center text-black/80">
           Hover to select items and then click to split a new range
         </div>
+        <div style={{ background: '#EFF6FF', padding: '1em', border: '1px solid #1E40AF', borderRadius: '4px' }}>
+          <InfoIcon fontSize={'18px'} height={30} fill={'#31539F'} style={{float: 'right'}} />
+          When creating an initial range for the selected manifest, two range items will be created: one containing the
+          items within the selected range and another containing the remaining items outside of that range
+        </div>
+
       </div>
     </SidebarContent>
   );
