a4cd008501446b7397dea8802c7ee2ff2d3efa88
Paul Mollahan <paul.mollahan.digirati@gmail.com>
2025-11-14 17:42:26 +0000

    Update title for adding-images in _meta.js

--- apps/docs/pages/docs/_meta.js
+++ apps/docs/pages/docs/_meta.js
@@ -1,8 +1,8 @@
 export default {
   "getting-started": { title: "Getting started" },
   "manifest-editing": { title: "Manifest editing" },
+  "adding-images": { title: "Creating a new Manifest" },
   "managing-workspaces": { title: "Managing workspaces" },
-  "adding-images": { title: "Adding images" },
   "adding-av": { title: "Adding audio and video" },
   "image-composition": { title: "Layering multiple images" },
   "creating-annotations": { title: "Creating annotations" },
