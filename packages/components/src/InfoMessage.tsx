69cfe32015f10935948f0ae2ffc72480f9024b13
Stephen Fraser <stephen.fraser@digirati.com>
2025-11-19 14:42:22 +0000

    Fixed info message colours

--- packages/components/src/InfoMessage.tsx
+++ packages/components/src/InfoMessage.tsx
@@ -7,16 +7,12 @@ interface InfoMessageProps {
   className?: string;
 }
 
-export function InfoMessage({
-  children,
-  small = false,
-  className,
-}: InfoMessageProps) {
+export function InfoMessage({ children, small = false, className }: InfoMessageProps) {
   return (
     <div
       className={twMerge(
-        "bg-blue-500 text-white",
-        "border-1 drop-shadow border-blue-700 w-full rounded-md py-2 px-4 leading-[1.9em] flex items-center",
+        "bg-me-500 text-white",
+        "border-1 drop-shadow border-me-700 w-full rounded-md py-2 px-4 leading-[1.9em] flex items-center",
         "has-[a]:text-white",
         small && "text-xs whitespace-nowrap py-1 px-2 leading-[1.6em] w-fit",
         className,
