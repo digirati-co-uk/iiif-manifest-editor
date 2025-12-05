fbd31e3f2893dc16e485aad604b66a6a58ea0414
Stephen Fraser <stephen.fraser@digirati.com>
2025-12-05 14:09:58 +0000

    Fixed external

--- apps/web/src/app/editor/external/page.tsx
+++ apps/web/src/app/editor/external/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import dynamic from "next/dynamic";
+import { use } from "react";
 
 const ExternalEditor = dynamic(() => import("../../../components/external-editor/ExternalEditor"), {
   ssr: false,
 });
 
-export default function ExternalEditorPage(props: { searchParams: { manifest: string; preset?: string } }) {
-  return <ExternalEditor manifest={props.searchParams.manifest} preset={props.searchParams.preset} />;
+export default function ExternalEditorPage(props: { searchParams: Promise<{ manifest: string; preset?: string }> }) {
+  const searchParams = use(props.searchParams);
+  return <ExternalEditor manifest={searchParams.manifest} preset={searchParams.preset} />;
 }
