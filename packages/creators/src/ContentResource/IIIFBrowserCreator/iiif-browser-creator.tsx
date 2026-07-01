93239ffe09d63e22c37ace53529365ce265df8d8
Stephen Fraser <stephen.fraser@digirati.com>
2026-07-01 14:59:07 +0100

    Fixed IIIF Browser creator to add partOf

--- packages/creators/src/ContentResource/IIIFBrowserCreator/iiif-browser-creator.tsx
+++ packages/creators/src/ContentResource/IIIFBrowserCreator/iiif-browser-creator.tsx
@@ -60,6 +60,20 @@ export async function createFromIIIFBrowserOutput(data: IIIFBrowserCreatorPayloa
 
         invariant(manifest, "Manifest not found");
 
+        const addManifestTracking = (resource: any): any => {
+          if (resource.type === "SpecificResource" && resource.source) {
+            return { ...resource, source: addManifestTracking(resource.source) };
+          }
+
+          return {
+            requiredStatement: manifest.requiredStatement,
+            rights: manifest.rights,
+            metadata: manifest.metadata || [],
+            ...resource,
+            partOf: [{ id: manifestId, type: "Manifest", label: manifest.label }],
+          };
+        };
+
         const canvasRef = manifest.items.find((item) => item.id === canvasId);
 
         invariant(canvasRef, "Canvas not found");
@@ -72,10 +86,8 @@ export async function createFromIIIFBrowserOutput(data: IIIFBrowserCreatorPayloa
             fullCanvas.label = { en: ["Untitled canvas"] };
           }
 
-          // @todo add partof manifest
-
           // Load before embedding.
-          ctx.vault.loadSync(fullCanvas.id, fullCanvas);
+          ctx.vault.loadSync(fullCanvas.id, addManifestTracking(fullCanvas));
           // Then embed.
           returnResources.push(ctx.embed({ id: fullCanvas.id, type: "Canvas" }));
           continue;
@@ -148,7 +160,7 @@ export async function createFromIIIFBrowserOutput(data: IIIFBrowserCreatorPayloa
             }
 
             if (targetType === "ContentResource") {
-              returnResources.push(fullAnnotation.body);
+              returnResources.push(addManifestTracking(fullAnnotation.body));
               continue;
             }
 
@@ -165,34 +177,38 @@ export async function createFromIIIFBrowserOutput(data: IIIFBrowserCreatorPayloa
               });
 
               returnResources.push(
-                ctx.embed({
-                  id: canvasId,
-                  type: "Canvas",
-                  label: { en: ["Untitled canvas"] },
-                  width: ~~selector.spatial.width,
-                  height: ~~selector.spatial.height,
-                  thumbnail: thumbnailId
-                    ? [
-                        ctx.embed({
-                          id: thumbnailId,
-                          type: "Image",
-                          format: "image/jpeg",
-                          width: 512,
-                          height: Math.round((selector.spatial.height / selector.spatial.width) * 512),
-                        }),
-                      ]
-                    : undefined,
-                  items: [annotationPage],
-                }),
+                ctx.embed(
+                  addManifestTracking({
+                    id: canvasId,
+                    type: "Canvas",
+                    label: { en: ["Untitled canvas"] },
+                    width: ~~selector.spatial.width,
+                    height: ~~selector.spatial.height,
+                    thumbnail: thumbnailId
+                      ? [
+                          ctx.embed({
+                            id: thumbnailId,
+                            type: "Image",
+                            format: "image/jpeg",
+                            width: 512,
+                            height: Math.round((selector.spatial.height / selector.spatial.width) * 512),
+                          }),
+                        ]
+                      : undefined,
+                    items: [annotationPage],
+                  }),
+                ),
               );
               continue;
             }
 
             returnResources.push(
-              ctx.embed({
-                ...fullAnnotation,
-                target: ctx.getTarget(),
-              }),
+              ctx.embed(
+                addManifestTracking({
+                  ...fullAnnotation,
+                  target: ctx.getTarget(),
+                }),
+              ),
             );
             continue;
           }
@@ -200,10 +216,12 @@ export async function createFromIIIFBrowserOutput(data: IIIFBrowserCreatorPayloa
           // @todo check return types (At the moment only annotation is supported.)
 
           returnResources.push(
-            ctx.embed({
-              ...fullAnnotation,
-              target: ctx.getTarget(),
-            }),
+            ctx.embed(
+              addManifestTracking({
+                ...fullAnnotation,
+                target: ctx.getTarget(),
+              }),
+            ),
           );
         }
       }
