525235e7943a77f493c203c62956afb792287c2c
Stephen Fraser <stephen.fraser@digirati.com>
2026-08-09 17:18:04 +0100

    fix: normalize dropdown hook module interop

--- packages/shell/src/use-dropdown-menu.ts
+++ packages/shell/src/use-dropdown-menu.ts
@@ -1,7 +1,11 @@
 import type useDropdownMenuType from "react-accessible-dropdown-menu-hook";
 import * as dropdownMenuModule from "react-accessible-dropdown-menu-hook";
 
-const imported = dropdownMenuModule as any;
-const useDropdownMenu = (imported.default?.default ?? imported.default ?? imported) as typeof useDropdownMenuType;
+export function resolveDropdownMenu(module: any): typeof useDropdownMenuType {
+  const defaultExport = Reflect.get(module, "default");
+  return (Reflect.get(defaultExport ?? {}, "default") ?? defaultExport ?? module) as typeof useDropdownMenuType;
+}
+
+const useDropdownMenu = resolveDropdownMenu(dropdownMenuModule);
 
 export default useDropdownMenu;
